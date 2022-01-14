7e4baaeadc8d8e8ed8bf774ccc6df80b5586c1a6
romesh-jaya <monty.rom.jay@gmail.com>
2022-01-14 10:25:08 +0530

    feat: select category page

--- src/components/SelectCategory.vue
+++ src/components/SelectCategory.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>This is Select Category</div>
+  <div class="container">
+    <div class="category-title">Select Category</div>
+    <SelectOption :options="categories" />
+  </div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
+import SelectOption from "./SelectOption.vue";
+import categoryData from "../sampleData/categories.json";
 
 export default defineComponent({
   name: "SelectCategory",
+  components: { SelectOption },
+  data() {
+    return {
+      categories: categoryData.categories,
+    };
+  },
 });
 </script>
-<style lang="scss"></style>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.category-title {
+  margin-block: 2rem;
+  font-size: 2rem;
+  font-weight: 900;
+}
+</style>
---
+++ src/components/SelectOption.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-options">
+    <div class="card" v-for="option in options" :key="option.id">
+      {{ option.value }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Option } from "../types/Option";
+
+export default defineComponent({
+  name: "SelectOption",
+  props: { options: Array as PropType<Array<Option>> },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/globals.scss";
+
+.container-options {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 2.5rem;
+  margin-inline: 2rem;
+}
+
+.card {
+  height: 10rem;
+  width: 10rem;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  background-color: bisque;
+  display: grid;
+  place-items: center;
+  font-size: 1.5rem;
+}
+
+.card:active {
+  color: $active-color;
+  box-shadow: 0 0 5px -1px rgba(0, 0, 0, 0.6);
+}
+</style>
