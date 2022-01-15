958ff1fc299778da6f05cfcf6491cbae19497e47
romesh-jaya <monty.rom.jay@gmail.com>
2022-01-15 12:30:48 +0530

    feat: item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
-    <div v-if="category" class="category-title">
-      Select Item: {{ category }}
+    <div v-if="itemId" class="category-title">
+      {{ item.value }}
     </div>
-    <div v-else class="category-title">Category not found</div>
-    <SelectOption :options="items" class="select-categories" />
+    <div v-else class="category-title">Item not found</div>
+    <div v-if="itemId" class="content">
+      <div class="image-div">
+        <img :src="item.thumbnailURL" class="item-image" />
+      </div>
+      <div class="description">{{ item.description }}</div>
+    </div>
     <div class="navbar-bottom">
       <NavButton @on-click="onBackClicked">BACK</NavButton>
     </div>
@@ -14,12 +19,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SelectOption from "./SelectOption.vue";
-import categoryData from "../sampleData/categories.json";
 import itemData from "../sampleData/items.json";
 import NavButton from "../common/NavButton.vue";
 
 export default defineComponent({
-  name: "SelectItem",
+  name: "Item",
   components: { SelectOption, NavButton },
   data() {
     return {};
@@ -30,20 +34,15 @@ export default defineComponent({
     },
   },
   computed: {
-    category() {
-      return categoryData.categories.find(
-        (category) => category.id === this.$route.params.categoryId
-      )?.value;
-    },
-    categoryId() {
-      return categoryData.categories.find(
-        (category) => category.id === this.$route.params.categoryId
-      )?.id;
-    },
-    items() {
-      return itemData.items.filter(
-        (item) => item.categoryId === this.$route.params.categoryId
+    item() {
+      return itemData.items.find(
+        (items) => items.id === this.$route.params.itemId
       );
+    },
+    itemId() {
+      return itemData.items.find(
+        (items) => items.id === this.$route.params.itemId
+      )?.id;
     },
   },
 });
@@ -69,5 +68,28 @@ export default defineComponent({
 .navbar-bottom {
   padding: 3rem;
   text-align: left;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  flex: 1 0px;
+}
+
+.image-div {
+  margin-block: 3rem;
+  height: 25rem;
+}
+
+.item-image {
+  height: 100%;
+  width: 80%;
+  object-fit: cover;
+  border-radius: 2rem;
+}
+
+.description {
+  font-size: 1.5rem;
+  font-weight: normal;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import Landing from "./components/Landing.vue";
 import SelectCategory from "./components/SelectCategory.vue";
 import SelectItem from "./components/SelectItem.vue";
+import Item from "./components/Item.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: Landing },
   { path: "/select-category", component: SelectCategory },
   { path: "/select-item/:categoryId", component: SelectItem },
+  { path: "/items/:itemId", component: Item },
 ];
 
 const router = createRouter({
