92959df6a3ad65f75b6dcaef3fcff8b30d926ca0
romesh-jaya <monty.rom.jay@gmail.com>
2022-01-27 09:46:46 +0530

    feat: add toast notification

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,48 @@ import { defineComponent } from "vue";
 import axios from "./axios";
 import { Category } from "./types/Category";
 import { Item } from "./types/Item";
+import { createToaster } from "@meforma/vue-toaster";
 
 const BE_URL = import.meta.env.VITE_BE_URL;
+const toaster = createToaster({
+  position: "top-right",
+  duration: 6000,
+});
 
 export default defineComponent({
   name: "App",
   mounted() {
     const getBasicData = async () => {
-      const response = await axios.get("/categories?populate=*");
-      const categoryData: Category[] = response.data.data.map(
-        (category: any) => {
+      try {
+        const response = await axios.get("/categories?populate=*");
+        const categoryData: Category[] = response.data.data.map(
+          (category: any) => {
+            return {
+              categoryId: category.attributes.categoryId,
+              categoryName: category.attributes.categoryName,
+              imageURL:
+                BE_URL +
+                category.attributes.image.data.attributes.formats.thumbnail.url,
+            };
+          }
+        );
+        const response2 = await axios.get("/items?populate=*");
+        const itemData: Item[] = response2.data.data.map((item: any) => {
           return {
-            categoryId: category.attributes.categoryId,
-            categoryName: category.attributes.categoryName,
-            imageURL:
-              BE_URL +
-              category.attributes.image.data.attributes.formats.thumbnail.url,
+            itemId: item.attributes.itemId,
+            itemName: item.attributes.itemName,
+            imageId: item.attributes.imageId,
+            categoryId: item.attributes.category.data.attributes.categoryId,
+            description: item.attributes.description,
+            price: item.attributes.price,
           };
-        }
-      );
-      const response2 = await axios.get("/items?populate=*");
-      const itemData: Item[] = response2.data.data.map((item: any) => {
-        return {
-          itemId: item.attributes.itemId,
-          itemName: item.attributes.itemName,
-          imageId: item.attributes.imageId,
-          categoryId: item.attributes.category.data.attributes.categoryId,
-          description: item.attributes.description,
-          price: item.attributes.price,
-        };
-      });
-      this.$store.commit("categories/saveCategories", categoryData);
-      this.$store.commit("items/saveItems", itemData);
+        });
+
+        this.$store.commit("categories/saveCategories", categoryData);
+        this.$store.commit("items/saveItems", itemData);
+      } catch {
+        toaster.error("Error in accessing backend server!");
+      }
     };
     getBasicData();
   },
@@ -100,5 +110,10 @@ p {
   padding: 2rem;
   display: flex;
   justify-content: space-between;
+}
+
+/* This is for the toast notifications */
+.c-toast {
+  font-family: "Nunito" !important;
 }
 </style>
